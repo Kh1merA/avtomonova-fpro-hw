46e0dbd9047095ca81f3adb86f7e0f568d3523f2
Автомонова Дар'я Сергіївна <104759810+Kh1merA@users.noreply.github.com>
2023-08-30 01:00:14 +0300

    fix hw2

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 
+let outElem = document.getElementsByClassName("result-massive")[0];
+
 function removeElement(arr, num){
-    arr.splice(arr.indexOf(num), 1);
+    if(arr.indexOf(num) === -1){
+        outElem.innerHTML = `Такого числа немає у масиві <br>`;
+    }
+    else {
+        arr.splice(arr.indexOf(num), 1);
+    }
 }
 
 const arr = [1, 2, 3, 4, 5, 6, 7];
 removeElement(arr,  5);
 
-let outElem = document.getElementsByClassName("result-massive")[0];
+outElem.innerHTML += `Результуючий масив: <br>`;
 
 arr.forEach((item) => {
     outElem.innerHTML += `${item} `;
 });
-
