50024c5ea5e1d56a8fd89bff01a7cb3d6a78d458
Автомонова Дар'я Сергіївна <104759810+Kh1merA@users.noreply.github.com>
2023-09-08 23:09:03 +0300

    complete hw2

---
+++ script.js
@@ -0,0 +1,18 @@
+let inputElement = document.getElementsByClassName('input-info')[0];
+let newElem = document.createElement('div');
+newElem.style.width = '380px';
+newElem.style.height = '571px';
+newElem.style.backgroundImage = 'url("img/landscape.jpg")';
+newElem.style.backgroundRepeat = 'no-repeat';
+inputElement.setAttribute('placeholder', 'Наведи на меня');
+inputElement.setAttribute('readonly', true);
+
+let mainElem = document.getElementsByClassName('wrapper')[0];
+
+inputElement.addEventListener('focus', () => {
+    mainElem.appendChild(newElem);
+});
+
+inputElement.addEventListener('focusout', () => {
+    mainElem.removeChild(newElem);
+})
