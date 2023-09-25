02454c17a495517803a7cac51b7500eaecae1623
Автомонова Дар'я Сергіївна <104759810+Kh1merA@users.noreply.github.com>
2023-09-25 22:53:12 +0300

    complete hw2

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
+function generateList(list_array){
+    let list_elem = document.createElement('ul');
+    list_elem.classList.add('list');
+
+    let array_counter = 1;
+
+    list_array.forEach(item => {
+        let list_li = document.createElement('li');
+        if(Array.isArray(item)){
+            let array_ul = document.createElement('ul');
+            item.forEach(item => {
+                let array_li = document.createElement('li');
+                array_li.textContent = array_counter + '.' + item.toString();
+                array_ul.append(array_li);
+            })
+            array_counter++;
+            list_li.append(array_ul);
+        }
+        else if (String(typeof(item)) === 'number'){
+            list_li.textContent = item.toString();
+        }
+        else{
+            list_li.textContent = 'Incorrect info';
+        }
+        list_elem.append(list_li);
+    });
+
+    document.getElementsByClassName('wrapper')[0].append(list_elem);
+}
+
+
 let list_array = [1, [1,2,3], 3, 4, [1, 2, 3]];
 
-let list_elem = document.createElement('ul');
-list_elem.classList.add('list');
+generateList(list_array);
 
-let array_counter = 1;
 
-list_array.forEach(item => {
-    let list_li = document.createElement('li');
-   if(Array.isArray(item)){
-       let array_ul = document.createElement('ul');
-       item.forEach(item => {
-           let array_li = document.createElement('li');
-           array_li.textContent = array_counter + '.' + item.toString();
-           array_ul.append(array_li);
-       })
-       array_counter++;
-       list_li.append(array_ul);
-   }
-   else if (String(typeof(item)) === 'number'){
-       list_li.textContent = item.toString();
-   }
-   else{
-       list_li.textContent = 'Incorrect info';
-   }
-   list_elem.append(list_li);
-});
-
-document.getElementsByClassName('wrapper')[0].append(list_elem);
