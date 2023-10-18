f2885f21e0d65ac7829b2483b5b82d9ae27f6a36
Автомонова Дар'я Сергіївна <104759810+Kh1merA@users.noreply.github.com>
2023-10-18 22:23:05 +0300

    fix

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const apiKey = '5d066958a60d315387d9492393935c19';
-    const city = 'Kharkiv';
-    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`;
+const apiKey = '5d066958a60d315387d9492393935c19';
+const city = 'Kharkiv';
+const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apiKey}`;
 
-    const temperatureElement = document.getElementById('temperature');
-    const pressureElement = document.getElementById('pressure');
-    const descriptionElement = document.getElementById('description');
-    const humidityElement = document.getElementById('humidity');
-    const windSpeedElement = document.getElementById('wind-speed');
-    const windDirectionElement = document.getElementById('wind-direction');
-    const weatherIconElement = document.getElementById('weather-icon');
+const temperatureElement = document.getElementById('temperature');
+const pressureElement = document.getElementById('pressure');
+const descriptionElement = document.getElementById('description');
+const humidityElement = document.getElementById('humidity');
+const windSpeedElement = document.getElementById('wind-speed');
+const windDirectionElement = document.getElementById('wind-direction');
+const weatherIconElement = document.getElementById('weather-icon');
 
-    // Виконуємо запит за допомогою fetch
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            // Оновлюємо відповідні елементи на сторінці з отриманими даними
-            temperatureElement.textContent = data.main.temp;
-            pressureElement.textContent = data.main.pressure;
-            descriptionElement.textContent = data.weather[0].description;
-            humidityElement.textContent = data.main.humidity;
-            windSpeedElement.textContent = data.wind.speed;
-            windDirectionElement.textContent = data.wind.deg;
-            weatherIconElement.src = `http://openweathermap.org/img/w/10d.png`;
-        })
-        .catch(error => {
-            console.error('Помилка при отриманні погоди: ', error);
-        });
-});
+// Виконуємо запит за допомогою fetch
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        // Оновлюємо відповідні елементи на сторінці з отриманими даними
+        temperatureElement.textContent = data.main.temp;
+        pressureElement.textContent = data.main.pressure;
+        descriptionElement.textContent = data.weather[0].description;
+        humidityElement.textContent = data.main.humidity;
+        windSpeedElement.textContent = data.wind.speed;
+        windDirectionElement.textContent = data.wind.deg;
+        weatherIconElement.src = `http://openweathermap.org/img/w/10d.png`;
+    })
+    .catch(error => {
+        console.error('Помилка при отриманні погоди: ', error);
+    });
